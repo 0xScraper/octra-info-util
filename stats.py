69d8b2dba3c896650e444194ef502b25c7ddb0a3
Sung JinWoo <openpy512@gmail.com>
2025-08-23 13:59:06 +0300

    IndexError fix in stats.py (epoch param)

--- stats.py
+++ stats.py
@@ -1,5 +1,6 @@
 # ruff: noqa: I001
 import os
+from pprint import pprint
 
 import requests
 from bs4 import BeautifulSoup
@@ -22,18 +23,19 @@ def status(url: str, *, flag: bool) -> None:
         soup: BeautifulSoup = BeautifulSoup(content, 'lxml')
         try:
             mono = soup.find_all('div', class_='mono')
-            param0, param1 = mono[3], mono[0]
+            # param0, param1 = mono[3], mono[0]
+            param0 = mono[0]
             if flag:
-                param2, param3 = mono[1], mono[2]
+                param1, param2, param3 = mono[1], mono[2], mono[3]
                 data: list[tuple[str]] = [
-                    (param0.text.upper(), param1.text),
-                    ('BALANCE', param2.text),
-                    ('NONCE', param3.text),
+                    (param3.text.upper(), param0.text),
+                    ('BALANCE', param1.text),
+                    ('NONCE', param2.text),
                 ]
                 table: str = tabulate(data, tablefmt='fancy_grid', colalign=('right', 'left'), missingval = 'N/A')
                 print(table)
             else:
-                print(tabulate([['EPOCH', param1.text], ], tablefmt='fancy_grid', colalign=('center', 'center'))) #noqa: COM819
+                print(tabulate([['EPOCH', param0.text], ], tablefmt='fancy_grid', colalign=('center', 'center'))) #noqa: COM819
         except IndexError as e:
             print(f'!!!!! Failed: {e}. \n!!!!! ERROR on the web-page !!!!!')
     else:
